f1f355b6a98498109ad35c8a4a5e85631042accb
user070917 <145314564+user070917@users.noreply.github.com>
2025-01-18 00:21:43 +0900

    restart

---
+++ Assets/Script/BackGround.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    private float startPos, length;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void FixedUpdate()
+    {
+        float distance = cam.transform.position.x * parallaxEffect;
+        float movement = cam.transform.position.x * (1 - parallaxEffect);
+
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if(movement > startPos + length)
+        {
+            startPos += length;
+        }
+        else if (movement < startPos - length)
+        {
+            startPos -= length;
+        }
+    }
+
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float horizontalInput;
+    [SerializeField]
+    float moveSpeed = 5f;
+    bool facingRight = true;
+    [SerializeField]
+    float jumpPower = 5f;
+    bool isGrounded = false;
+
+    Rigidbody2D rb;
+    Animator ani;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        FlipSprite();
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            isGrounded = false;
+            ani.SetBool("IsJumping", !isGrounded);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+        ani.SetFloat("Xvelocity", Math.Abs(rb.velocity.x));
+        ani.SetFloat("Yvelocity", rb.velocity.y);
+    }
+
+
+    void FlipSprite()
+    {
+        if (facingRight && horizontalInput < 0f || !facingRight && horizontalInput > 0f)
+        {
+            facingRight = !facingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isGrounded = true;
+        ani.SetBool("IsJumping", !isGrounded);
+    }
+
+}
