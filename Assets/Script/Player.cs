d45a6ad0d651881bf95ed7650c731f1c2d3b139d
user070917 <145314564+user070917@users.noreply.github.com>
2025-01-18 22:26:04 +0900

    더블 점프 및 이동 속도 정상화

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -9,14 +9,16 @@ public class Player : MonoBehaviour
     [SerializeField]
     float moveSpeed = 5f;
     bool facingRight = true;
+    bool isGrounded = false;
     [SerializeField]
     float jumpPower = 5f;
-    bool isGrounded = false;
+    int jumpCount = 0; // 점프 횟수 추적
+    [SerializeField]
+    int maxJumpCount = 2; // 최대 점프 횟수
     [SerializeField]
     float curtime;
     [SerializeField]
     float cooltime = 0.5f;
- 
 
     Rigidbody2D rb;
     Animator ani;
@@ -27,7 +29,6 @@ public class Player : MonoBehaviour
         ani = GetComponent<Animator>();
     }
 
-
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
@@ -35,36 +36,47 @@ public class Player : MonoBehaviour
         FlipSprite();
 
         // 점프 처리
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && jumpCount < maxJumpCount)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
-            isGrounded = false;
-            ani.SetBool("IsJumping", !isGrounded);
+            jumpCount++;
+            ani.SetBool("IsJumping", true);
         }
 
         // 공격 처리
-        if (Input.GetKey(KeyCode.Q) && isGrounded)  // Q가 눌려있으면 계속 공격
+        if (Input.GetKey(KeyCode.Q) && jumpCount == 0) // Q가 눌려있으면 계속 공격
         {
             if (curtime <= 0)
             {
-                ani.SetBool("Attack", true);  // 공격 상태 유지, 시발 루프 때매 한시간을 쓰네 개같은거
-                curtime = cooltime;  // 쿨타임 갱신
+                ani.SetBool("Attack", true); // 공격 상태 유지
+                curtime = cooltime; // 쿨타임 갱신
             }
         }
         else
         {
-            ani.SetBool("Attack", false);  // Q를 떼면 공격 상태 종료
+            ani.SetBool("Attack", false); // Q를 떼면 공격 상태 종료
         }
 
         if (curtime > 0)
         {
-            curtime -= Time.deltaTime;  // 쿨타임 감소
+            curtime -= Time.deltaTime; // 쿨타임 감소
         }
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+        float AdjustSpeed = moveSpeed;
+
+        if (ani.GetBool("Attack"))
+        {
+            AdjustSpeed *= 0.3f;
+        }
+        if (ani.GetBool("IsJumping"))
+        {
+            AdjustSpeed *= 0.7f;
+        }
+
+        rb.velocity = new Vector2(horizontalInput * AdjustSpeed, rb.velocity.y);
         ani.SetFloat("Xvelocity", Math.Abs(rb.velocity.x));
         ani.SetFloat("Yvelocity", rb.velocity.y);
     }
@@ -82,10 +94,9 @@ public class Player : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // 땅에 닿았을 때 점프 카운트 초기화
         isGrounded = true;
-        ani.SetBool("IsJumping", !isGrounded);
+        jumpCount = 0;
+        ani.SetBool("IsJumping", false);
     }
-
-
-
 }
