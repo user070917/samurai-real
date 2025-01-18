76cacae982007297c715eb7c0309d27dfd1ebc2b
user070917 <145314564+user070917@users.noreply.github.com>
2025-01-18 21:46:54 +0900

    땅공격 완성

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -12,6 +12,11 @@ public class Player : MonoBehaviour
     [SerializeField]
     float jumpPower = 5f;
     bool isGrounded = false;
+    [SerializeField]
+    float curtime;
+    [SerializeField]
+    float cooltime = 0.5f;
+ 
 
     Rigidbody2D rb;
     Animator ani;
@@ -29,11 +34,31 @@ public class Player : MonoBehaviour
 
         FlipSprite();
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        // 점프 처리
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
             isGrounded = false;
             ani.SetBool("IsJumping", !isGrounded);
+        }
+
+        // 공격 처리
+        if (Input.GetKey(KeyCode.Q) && isGrounded)  // Q가 눌려있으면 계속 공격
+        {
+            if (curtime <= 0)
+            {
+                ani.SetBool("Attack", true);  // 공격 상태 유지, 시발 루프 때매 한시간을 쓰네 개같은거
+                curtime = cooltime;  // 쿨타임 갱신
+            }
+        }
+        else
+        {
+            ani.SetBool("Attack", false);  // Q를 떼면 공격 상태 종료
+        }
+
+        if (curtime > 0)
+        {
+            curtime -= Time.deltaTime;  // 쿨타임 감소
         }
     }
 
@@ -44,7 +69,6 @@ public class Player : MonoBehaviour
         ani.SetFloat("Yvelocity", rb.velocity.y);
     }
 
-
     void FlipSprite()
     {
         if (facingRight && horizontalInput < 0f || !facingRight && horizontalInput > 0f)
@@ -54,7 +78,6 @@ public class Player : MonoBehaviour
             ls.x *= -1f;
             transform.localScale = ls;
         }
-
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -62,5 +85,7 @@ public class Player : MonoBehaviour
         isGrounded = true;
         ani.SetBool("IsJumping", !isGrounded);
     }
+
+
 
 }
